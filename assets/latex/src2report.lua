9833ad0057078a591069449fe5885efeb3365c35
Reiner Huechting <dev@reinerhuechting.de>
2025-01-27 20:17:28 +0100

    refactor: Use an object oriented approach in the lua code.

--- assets/latex/src2report.lua
+++ assets/latex/src2report.lua
@@ -1,63 +1,71 @@
-function src2report_init()
-    src2report_tasks = {}
-    src2report_students = {}
+Report = {
+    tasks = {},
+    students = {},
+    base_dir = ".",
+    submission_dir = "submissions",
+    src_file_ext = ".go"
+}
 
-    src2report_base_dir = "."
-    src2report_submission_dir = "submissions"
-    src2report_src_file_ext = ".go"
+function Report:new(o)
+    o = o or {}
+    setmetatable(o, self)
+    self.__index = self
+    return o
 end
 
-function src2report_set_base_dir(base_dir)
-    src2report_base_dir = base_dir
+function Report:set_base_dir(base_dir)
+    self.base_dir = base_dir
 end
 
-function src2report_set_submission_dir(submission_dir)
-    src2report_submission_dir = submission_dir
+function Report:set_submission_dir(submission_dir)
+    self.submission_dir = submission_dir
 end
 
-function src2report_add_task(dir, name)
+function Report:add_task(dir, name)
     local entry = {dir = dir, name = name}
-    table.insert(src2report_tasks, entry)
+    table.insert(self.tasks, entry)
 end
 
-function src2report_add_student(dir, name)
+function Report:add_student(dir, name)
     local entry = {dir = dir, name = name}
-    table.insert(src2report_students, entry)
+    table.insert(self.students, entry)
 end
 
-function src2report_print_task_report(task_name, file_path)
+function Report:print_task_reports(submission_dir)
+    for _, entry in ipairs(self.tasks) do
+        local task_file = self:task_path(submission_dir, entry.dir)
+        local task_name = self:escape_tex(entry.name)
+        self:print_task_report(task_name, task_file)
+    end
+end
+
+function Report:print_submission_report(student_name, submission_dir)
+    tex.sprint("\\submissionreporthead{" .. student_name .. "}")
+    self:print_task_reports(submission_dir)
+end
+
+function Report:print_submission_reports()
+    for _, entry in ipairs(self.students) do
+        local submission_dir = self:submission_path(entry.dir)
+        local student_name = self:escape_tex(entry.name)
+        self:print_submission_report(student_name, submission_dir)
+    end
+end
+
+function Report:print_task_report(task_name, file_path)
     tex.sprint("\\taskreport{" .. task_name .. "}{" .. file_path .. "}")
 end
 
-function src2report_print_task_reports(submission_dir)
-    for _, entry in ipairs(src2report_tasks) do
-        local task_file = src2report_task_path(submission_dir, entry.dir)
-        local task_name = src2report_escape_tex(entry.name)
-        src2report_print_task_report(task_name, task_file)
-    end
-end
-
-function src2report_print_submission_report(student_name, submission_dir)
-    tex.sprint("\\submissionreporthead{" .. student_name .. "}")
-    src2report_print_task_reports(submission_dir)
-end
-
-function src2report_print_submission_reports()
-    for _, entry in ipairs(src2report_students) do
-        local submission_dir = src2report_submission_path(entry.dir)
-        local student_name = src2report_escape_tex(entry.name)
-        src2report_print_submission_report(student_name, submission_dir)
-    end
-end
-
-function src2report_escape_tex(str)
+function Report:escape_tex(str)
     return string.gsub(str, "_", "\\_")
 end
 
-function src2report_task_path(submission_dir, task_name)
-    return submission_dir .. "/" .. task_name .. "/" .. task_name .. src2report_src_file_ext
+function Report:task_path(submission_dir, task_name)
+    return submission_dir .. "/" .. task_name .. "/" .. task_name .. self.src_file_ext
 end
 
-function src2report_submission_path(submission_dirname)
-    return src2report_base_dir .. "/" .. src2report_submission_dir .. "/" .. submission_dirname
+function Report:submission_path(submission_dirname)
+    return self.base_dir .. "/" .. self.submission_dir .. "/" .. submission_dirname
 end
+
+report = Report:new()
